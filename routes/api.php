ff75082d9e46a08bd1fab3269fa016ff9dd85f4f
dulithamahishka94 <dulithamahishka94@gmail.com>
2022-09-05 20:27:45 +0530

    updated routes

--- routes/api.php
+++ routes/api.php
@@ -28,18 +28,20 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']);
 
-// Forum
-Route::middleware('auth:api')->post('/create-forum', [ForumController::class, 'create']);
-Route::middleware('auth:api')->post('/delete-forum', [ForumController::class, 'delete']);
-Route::middleware('auth:api')->post('/list', [ForumController::class, 'index']);
+Route::group(['middleware' => 'auth:api'], function() {
+    // Forum
+    Route::post('/create-forum', [ForumController::class, 'create']);
+    Route::post('/delete-forum', [ForumController::class, 'delete']);
+    Route::post('/list', [ForumController::class, 'index']);
 
-// Comments
-Route::middleware('auth:api')->post('/create-comment', [CommentController::class, 'create']);
-Route::middleware('auth:api')->post('/view-comments', [CommentController::class, 'get']);
+    // Comments
+    Route::post('/create-comment', [CommentController::class, 'create']);
+    Route::post('/view-comments', [CommentController::class, 'get']);
 
-// Approvals
-Route::middleware('auth:api')->post('/approve', [ApproveController::class, 'approve']);
-Route::middleware('auth:api')->post('/reject', [ApproveController::class, 'reject']);
+    // Approvals
+    Route::post('/approve', [ApproveController::class, 'approve']);
+    Route::post('/reject', [ApproveController::class, 'reject']);
+});
 
 Route::fallback(function(){
     return response()->json([
